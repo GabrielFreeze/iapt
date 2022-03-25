53b7638b63e91cbbac28e9409bb3d1f31d7b94e0
Gabriel_Freeze <gabriel.hili.20@um.edu.mt>
2022-03-25 18:24:26 +0100

    asdasd

--- src/dataframe_generation.py
+++ src/dataframe_generation.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import os
 import json
-from pandas import json_normalize
 
-
-path = os.path.join(os.path.dirname(__file__),'..','data','gabra','lexemes.json')
+path = os.path.join(os.path.dirname(__file__),'..','data','gabra','sources.json')
 df = pd.DataFrame()
 
-# with open(path) as f:
-#     for l in f.readlines():
-#         df_tmp =         
+with open(path) as f:
+    lines = f.readlines()
     
+    
+    for i,l in enumerate(lines):
+        
+        if len(l) != 1:
+            json_object = json.loads(l)
+            df_tmp = pd.json_normalize(json_object)
+            df = pd.concat([df,df_tmp])
+
+print(df.head())
