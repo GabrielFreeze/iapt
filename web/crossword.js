ecda6d64585fe3058d76a6a44cf90d06c9c6b9d6
Gabriel_Freeze <81355262+GabrielFreeze@users.noreply.github.com>
2022-05-02 00:56:42 +0200

    Im a simpleton

--- web/crossword.js
+++ web/crossword.js
@@ -1,5 +1,5 @@
 function setLetter(i,j,letter) {
-    w = 8
+    w = 10
     index = i+(j*w)
 
     document.getElementById(index.toString()).innerHTML = letter.toString()
@@ -7,7 +7,66 @@ function setLetter(i,j,letter) {
 
 
 function placeCursor(i,direction) {
+    
+    
     //Place the cursor on the tile the clue begins in.
     //Set it so when the user types something the cursor goes to the appropriate square.
     
 }
+
+
+function setTile(i,j,state) {
+    w = 10
+    index = j+(i*w)+1
+    div = document.getElementById('letter-'+index.toString())
+
+    if (state == 'blank') {
+        //Make background color of div to black
+        div.style.backgroundColor = 'black'
+        
+        //Remove text from div
+        div.innerHTML = ' '
+    } else if (state == 'filled') {
+
+        //TODO: Make number appear on starting tiles only.
+
+
+        
+        
+
+        //Set number on starting tile.
+    }
+
+
+
+ 
+}
+
+
+function initGrid() {
+
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            setTile(i,j,(grid[i][j] == ' ')? 'blank':'filled')
+        }
+    }
+
+}
+
+var grid = [
+    /*  0   1   2   3   4   5   6   7   8   9 */
+ /*0*/[' ','m','e','x','x','a',' ',' ',' ',' '],/*0*/
+ /*1*/[' ',' ',' ',' ','i','j',' ',' ',' ',' '],/*1*/
+ /*2*/[' ',' ','r','i','k','k','i','e','b',' '],/*2*/
+ /*3*/[' ',' ',' ','b','e','l','u','l','a',' '],/*3*/
+ /*4*/[' ',' ',' ',' ','l','a',' ','m','j',' '],/*4*/
+ /*5*/[' ',' ',' ',' ',' ',' ',' ','u','d',' '],/*5*/
+ /*6*/[' ',' ',' ',' ',' ',' ',' ',' ','a',' '],/*6*/
+ /*7*/[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],/*7*/
+ /*8*/[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],/*8*/
+ /*9*/[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],/*9*/
+    /*  0   1   2   3   4   5   6   7   8   9 */
+    ];
+
+    var wordlist = [[[0,1],[2,2],[3,3]],
+                    [0,4],[0,5],[2,7],[8,2]]
